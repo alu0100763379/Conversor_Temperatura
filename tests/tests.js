a0b0a4e6d0ac8227e8dc1092ce71404b0f03cde4
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-10-08 18:16:25 +0100

    añadido kelvin a la transformación de C y F)

--- tests/tests.js
+++ tests/tests.js
@@ -4,12 +4,14 @@ suite('temperature', function() {
     test('32F = 0C', function() {
         original.value = "32F";
         calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        assert.deepEqual(converted1.innerHTML, "0.0 Celsius");
+	//assert.deepEqual(converted2.innerHTML, "273.1 Kelvin");
     });
     test('45C = 113.0 Farenheit', function() {
         original.value = "45C";
         calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        assert.deepEqual(converted1.innerHTML, "113.0 Farenheit");
+	//assert.deepEqual(converted2.innerHTML, "318.1 Kelvin");
     });
     test('5X = error', function() {
         original.value = "5X";
