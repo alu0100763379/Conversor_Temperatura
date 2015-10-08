b33183100faf3b134a9959861c46fffa9033ca04
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-10-08 19:42:36 +0100

    merge con Kelvin

--- tests/tests.js
+++ tests/tests.js
@@ -5,13 +5,13 @@ suite('temperature', function() {
         original.value = "32F";
         calculate();
         assert.deepEqual(converted1.innerHTML, "0.0 Celsius");
-	//assert.deepEqual(converted2.innerHTML, "273.1 Kelvin");
+	    assert.deepEqual(converted2.innerHTML, "273.1 Kelvin");
     });
     test('45C = 113.0 Farenheit', function() {
         original.value = "45C";
         calculate();
         assert.deepEqual(converted1.innerHTML, "113.0 Farenheit");
-	//assert.deepEqual(converted2.innerHTML, "318.1 Kelvin");
+	    assert.deepEqual(converted2.innerHTML, "318.1 Kelvin");
     });
     test('5X = error', function() {
         original.value = "5X";
